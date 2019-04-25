10e4d4f2d48ff0596851256264ffcd47488d2469
gderick-1 <godsonderick@gmail.com>
2019-04-26 01:10:34 +0300

    site components created

---
+++ src/app/kilimanjaro/kilimanjaro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kilimanjaro',
+  templateUrl: './kilimanjaro.component.html',
+  styleUrls: ['./kilimanjaro.component.css']
+})
+export class KilimanjaroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
