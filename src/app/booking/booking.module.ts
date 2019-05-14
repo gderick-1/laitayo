fd4ba0afb524ab48b9a0f62658c003a77da1d26e
gderick-1 <godsonderick@gmail.com>
2019-05-14 12:36:40 +0300

    booking form modified

--- src/app/booking/booking.module.ts
+++ src/app/booking/booking.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { BookingRoutingModule } from './booking-routing.module';
 import { BookingComponent } from "./booking.component";
+
 
 @NgModule({
   declarations: [ BookingComponent],
   imports: [
     CommonModule,
-    BookingRoutingModule
+    BookingRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class BookingModule { }
