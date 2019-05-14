9716f2605bf109f9f487a9a7dbca15001d9a379c
gderick-1 <godsonderick@gmail.com>
2019-05-14 12:35:48 +0300

    contact form modified

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  contactForm = this.fbLaitayo.group({
+    'name': [''],
+    'email': ['',  Validators.compose([
+      Validators.required,
+  	  Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+    ])],
+    'phone': ['', Validators.compose([
+      Validators.required,
+  	  Validators.pattern('/^\+(?:[0-9] ?){6,14}[0-9]$/')
+    ])],
+    'message': ['']
+  });
+  
+  constructor( private fbLaitayo: FormBuilder) {}
+  ngOnInit() {}
+  onSubmit(){
+    // display form values when submit button clicked
+    console.warn(this.contactForm.value);
   }
 
 }
--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { ContactRoutingModule } from './contact-routing.module';
 import { ContactComponent } from "./contact.component";
+
 
 @NgModule({
   declarations: [ ContactComponent ],
   imports: [
     CommonModule,
-    ContactRoutingModule
+    ContactRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class ContactModule { }
