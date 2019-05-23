9b7cd4b3866170bbf7fd1861f91aefeac7e3de19
gderick-1 <godsonderick@gmail.com>
2019-05-23 14:38:28 +0300

    deletes imported components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,39 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 // import { AgmCoreModule } from '@agm/core';
-
-// import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from "./layout/layout.module";
-// import { NgorongoroComponent } from './ngorongoro/ngorongoro.component';
-// import { SerengetiComponent } from './serengeti/serengeti.component';
-// import { TarangireComponent } from './tarangire/tarangire.component';
-// import { ManyaraComponent } from './manyara/manyara.component';
-// import { ZanzibarComponent } from './zanzibar/zanzibar.component';
-// import { KilimanjaroComponent } from './kilimanjaro/kilimanjaro.component';
-// import { ContactComponent } from './contact/contact.component';
-// import { HomeComponent } from './home/home.component';
-// import { TravelguideComponent } from './travelguide/travelguide.component';
-// import { BookingComponent } from './booking/booking.component';
-
 @NgModule({
   declarations: [
     AppComponent,
-    // NgorongoroComponent,
-    // SerengetiComponent,
-    // TarangireComponent,
-    // ManyaraComponent,
-    // ZanzibarComponent,
-    // KilimanjaroComponent,
-    // ContactComponent,
-    // HomeComponent,
-    // TravelguideComponent,
-    // BookingComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([]),
-    // AppRoutingModule,
     LayoutModule
     // AgmCoreModule.forRoot({
     //   apiKey: ''
