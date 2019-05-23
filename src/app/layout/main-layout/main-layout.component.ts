29ddd0492f6df2694265179de69d64b120393a4b
gderick-1 <godsonderick@gmail.com>
2019-05-23 14:39:19 +0300

    footer modified

--- src/app/layout/main-layout/main-layout.component.ts
+++ src/app/layout/main-layout/main-layout.component.ts
@@ -7,7 +7,7 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./main-layout.component.css']
 })
 export class MainLayoutComponent implements OnInit {
-
+  
   constructor( private route: ActivatedRoute) {
     console.log('main layout component constructor called');
    }
@@ -16,6 +16,8 @@ export class MainLayoutComponent implements OnInit {
     this.route.data.subscribe(data => {
       console.log(data);
     })
+
+    
   }
 
 }
