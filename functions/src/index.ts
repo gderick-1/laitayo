1bfab955c740058115d79676e89f8c58a4aa1a3d
gderick-1 <godsonderick@gmail.com>
2019-06-04 16:27:02 +0300

    sendgrid implemented

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,14 @@
 'use strict';
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
-import * as nodemailer from 'nodemailer';
-// import { Change } from 'firebase-functions';
+// import * as SENDGRID_API_KEY from '../sendgrid.env';
+// import * as nodemailer from 'nodemailer';
+
 
 // initialize firebase Admin SDK to access firebase Realtime Database
 admin.initializeApp(functions.config().firebase);
+
+// var db = admin.firestore();
 
 // Take the text parameter passed to this HTTP endpoint and insert it into the
 // Realtime Database under the path /messages/:pushId/original
@@ -18,41 +21,116 @@ exports.addMessage = functions.https.onRequest(async (req, res) => {
     res.redirect(303, snapshot.ref.toString());
   });
 
+// sendgrid webhook implementation process.env.SENDGRID_API_KEY
+// const apiKEY = require('../sendgrid.env');
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const gmailEmail = encodeURIComponent(functions.config().gmail.email);
-const gmailPassword = encodeURIComponent(functions.config().gmail.password);
-const mailTransport = nodemailer.createTransport(`smtps://${gmailEmail}:${gmailPassword}@smtp.gmail.com`);
+// const gmailPassword = encodeURIComponent(functions.config().gmail.password);
+// const mailTransport = nodemailer.createTransport(`smtps://${gmailEmail}:${gmailPassword}@smtp.gmail.com`);
+
+// exports.sendBookingEmail = functions.firestore.document('/booking/{bookId}').onCreate( (snap)  =>{
+//   const newData = snap.data();
+
+//   const name = newData!.name;
+//   const email = newData!.email;
+//   const message = newData!.message;
+//   const arrival = newData!.arrivalDate;
+//   const depart = newData!.departureDate;
+//   const phone = newData!.phone;
+//   const kid = newData!.child;
+//   const adult = newData!.adults;
+
+//   const msg = {
+//     to: gmailEmail,
+//     from: `${email}`,
+//     subject: `Booking request from ${name}`,
+//     text: `Your message content here\n
+//             Subject: Booking request from ${name}\n
+//             Sender's Name: ${name} \n
+//             Content: ${message}. \n\nWe expect to have ${adult} adult(s) and ${kid} kid(s)\n
+//             This tour will take place from 
+//             ${arrival} to ${depart}\n
+//             for any clarification or information regarding this booking call us through ${phone}`,
+//     html: `'<strong>from ${arrival} to ${depart}</strong>'`,
+//   };
+//   sgMail.send(msg);
+// });
+
+// exports.sendContactMessage = functions.firestore.document('/users/{userId}').onCreate((snap, context) =>{
+//   const contactData = snap.data();
+
+//   const name = contactData!.name;
+//   const email = contactData!.email;
+//   const phone = contactData!.phone;
+//   const message = contactData!.message;
+
+//   const msg = {
+//     to: gmailEmail,
+//     from: email,
+//     subject: `" contact from ${name}"`,
+//     text: `${message}\n\n
+//           Call: ${phone}`,
+//   };
+//   sgMail.send(msg);
+// });
 
 //submit booking notice from booking components
 exports.sendContactMessage = functions.database.ref('/booking/{pushKey}').onWrite((change) =>{
-    const dataAfterChange = change.after.val();
+    const newData = change.after.val();
     // only send emails for new messages
-    if (change.before.val() || !dataAfterChange.name) {
+    if (change.before.val() || !newData.name) {
         return;
     }
 
-    const val = dataAfterChange;
+    const val = newData;
 
-    const mailOptions = {
-        to: gmailEmail,
-        from: `${val.email}`,
-        subject: `Booking request from ${val.name}`,
-        text:`Your message content here\n
-             Subject: Booking request from ${val.name}\n
-             Sender's Name: ${val.name} \n
-             Content: ${val.message}.\n
-             This tour will take place from 
-             ${val.arrivalDate} to ${val.departureDate}\n
-             for any clarification or cancellation of your booking call us through ${val.phone}`
-    }
-    return mailTransport.sendMail(mailOptions).then((snapshot) =>{
+    const name = val.name;
+    const email = val.email;
+    const message = val.message;
+    const arrival = val.arrivalDate;
+    const depart = val.departureDate;
+    const phone = val.phone;
+    const kid = val.child;
+    const adult = val.adults;
+  
+    const msg = {
+      to: gmailEmail,
+      from: `${email}`,
+      subject: `Booking request from ${name}`,
+      text: `Your message content here\n
+              Subject: Booking request from ${name}\n
+              Sender's Name: ${name} \n
+              Content: ${message}. \n\nWe expect to have ${adult} adult(s) and ${kid} kid(s)\n
+              This tour will take place from 
+              ${arrival} to ${depart}\n
+              for any clarification or information regarding this booking call us through ${phone}`,
+      html: `'<strong>from ${arrival} to ${depart}</strong>'`,
+    };
+    sgMail.send(msg);
+      
+
+    // const mailOptions = {
+    //     to: `${val.email}`,
+    //     from: gmailEmail,
+    //     subject: `Booking request from ${val.name}`,
+        // text:`Your message content here\n
+        //      Subject: Booking request from ${val.name}\n
+        //      Sender's Name: ${val.name} \n
+        //      Content: ${val.message}.\n
+        //      This tour will take place from 
+        //      ${val.arrivalDate} to ${val.departureDate}\n
+        //      for any clarification or cancellation of your booking call us through ${val.phone}`
+    // }
+    // return mailTransport.sendMail(mailOptions).then((snapshot) =>{
          
-        console.log('Mail sent:', snapshot);
-        return 1;
-    }).catch( error => {
-        console.error('Error:', error);
-        return 1;
-    });
+    //     console.log('Mail sent:', snapshot);
+    //     return 1;
+    // }).catch( error => {
+    //     console.error('Error:', error);
+    //     return 1;
+    // });
 });
 
 
@@ -67,33 +145,30 @@ exports.addMessage = functions.https.onRequest(async (req, res) => {
     res.redirect(303, snapshot.ref.toString());
   });
 
-// contact us for  detailed service worksheet
+// // contact us for  detailed service worksheet
 exports.sendContactMessage = functions.database.ref('/users/{pushKey}').onWrite((contact) =>{
-    const dataAfterContact = contact.after.val();
+    const contactData = contact.after.val();
 
     //only send emails for new messages
-    if (contact.before.val() || !dataAfterContact.name) {
+    if (contact.before.val() || !contactData.name) {
         return;
     }
 
-    const cont = dataAfterContact;
-    const mailOptions = {
-        to: gmailEmail,
-        from: `${cont.email}`,
-        subject: `'Feedback from ${cont.name} '`,
-        text: `Message content here\n
-              Subject: Message from ${cont.name} \n
-              Sender's Name: ${cont.name}\n
-              Content: ${cont.message}`
-    }
-    return mailTransport.sendMail(mailOptions).then((snap) =>{
-        
-        console.log('feedback sent:', snap); 
-        return 1;  
-    }).catch(error =>{
-        console.error('Error:', error);
-        return 1;
-    });
+    const cont = contactData;
+    
+  const name = cont.name;
+  const email = cont.email;
+  const phone = cont.phone;
+  const message = cont.message;
+
+  const msg = {
+    to: gmailEmail,
+    from: email,
+    subject: `" contact from ${name}"`,
+    text: `${message}\n\n
+          Call: ${phone}`,
+  };
+  sgMail.send(msg);
 })
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
